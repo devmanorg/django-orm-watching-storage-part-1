6df1b1ef88e6b2ba97fa307371128293be61cd11
Vlad <dr.melix@gmail.com>
2024-05-11 17:27:17 +0300

    Продолжаю выполнять шаги задания: Шаг 2 из 18 Выберите один пропуск Шаг 3 из 18 Найдите все действующие карточки Шаг 4 из 18 Отфильтруйте базу данных Шаг 5 из 18 Запустите сайт Шаг 6 из 18 Покажите активные пропуски Шаг 7 из 18 Получите визиты пользователей Шаг 8 из 18 Узнайте кто сейчас в хранилище Шаг 9 из 18 Узнайте сколько времени посетители провели в хранилище Шаг 10 из 18

--- datacenter/models.py
+++ datacenter/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Passcard(models.Model):
-    objects = None
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now=True)
     passcode = models.CharField(max_length=200, unique=True)
--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 import os
 import django
+from django.utils import timezone
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard
+from datacenter.models import Passcard, Visit
+
+active_passcard = Passcard.objects.filter(is_active=True)
 
 if __name__ == '__main__':
-    # Программируем здесь
+    # Получаем только активные пропуска
+    active_passcards = Passcard.objects.filter(is_active=True)
+
+    # Выводим информацию
     print('Количество пропусков:', Passcard.objects.count())
+    print('Активных пропусков:', active_passcards.count())
+    print()
+    print('Получите визиты пользователей', Visit.objects.all())
+    print()
+    print('Пользователи в хранилище', Visit.objects.filter(leaved_at__isnull=True))
+    print()
+    # Выводим информацию о первом активном пропуске
+    first_active_passcard = active_passcards.first()
+    print(f"owner_name: {first_active_passcard.owner_name}")
+    print(f"passcode: {first_active_passcard.passcode}")
+    print(f"created_at: {first_active_passcard.created_at.strftime('%Y-%m-%d %H:%M:%S.%f')}")
+    print(f"is_active: {first_active_passcard.is_active}")
+    print()
+    # Сколько времени посетители провели в хранилище
+    active_visits = Visit.objects.filter(leaved_at__isnull=True)
+    for visit in active_visits:
+        entered_at = timezone.localtime(visit.entered_at)
+        now = timezone.localtime(timezone.now())
+        duration = now - entered_at
+        print(f'Зашёл в хранилище, время по Москве: {entered_at}')
+        print(f'Находится в хранилище: {duration}')
+
+
+
+
+
